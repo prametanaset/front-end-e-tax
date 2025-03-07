ddeb352378588f20e11607a8915937f50fac5198
Bastaks14096 <taksin.k@kkumail.com>
2025-03-07 10:54:23 +0700

    fix button component

--- components/Button.vue
+++ components/Button.vue
@@ -1,3 +1,13 @@
 <template>
+  <div>
     <button class="mt-6 text-white bg-blue-700 p-5 cursor-pointer" @click="console.log('click')">Click Me</button>
+  </div>
 </template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
