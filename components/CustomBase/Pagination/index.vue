bc5f655c4839d68441c9970e2a25cb99407ceb32
MSI\prame <prametanaset147@gmail.com>
2025-03-11 16:13:52 +0700

    create new pagination component

---
+++ components/CustomBase/Pagination/index.vue
@@ -0,0 +1,357 @@
+<script setup lang="ts">
+import type { RouteLocationRaw } from 'vue-router'
+
+const props = withDefaults(
+  defineProps<{
+    /**
+     * The number of items to display per page.
+     */
+    itemPerPage: number
+
+    /**
+     * The total number of items.
+     */
+    totalItems: number
+
+    /**
+     * The current page number.
+     */
+    currentPage?: number
+
+    /**
+     * The maximum number of links to display.
+     */
+    maxLinksDisplayed?: number
+
+    /**
+     * Whether to disable routing.
+     */
+    noRouter?: boolean
+
+    /**
+     * The query key to use for routing.
+     */
+    routerQueryKey?: string
+
+    /**
+     * The icon to show for the previous button.
+     */
+    previousIcon?: string
+
+    /**
+     * The icon to show for the next button.
+     */
+    nextIcon?: string
+
+    /**
+     * The icon to show for the first button.
+     */
+    firstIcon?: string
+
+    /**
+    * The icon to show for the last button.
+    */
+    lastIcon?: string
+
+    /**
+     * The ellipsis to show when there are too many links.
+     */
+    ellipsis?: string
+
+    /**
+     * The color of the pagination active button.
+     *
+     * @since 3.0.0
+     * @default 'primary'
+     */
+    color?: 'primary' | 'dark' | 'black'
+
+    /**
+     * The radius of the pagination.
+     *
+     * @since 2.0.0
+     * @default 'sm'
+     */
+    rounded?: 'none' | 'sm' | 'md' | 'lg' | 'full'
+
+    /**
+     * Optional CSS classes to apply to the component inner elements.
+     */
+    classes?: {
+      /**
+       * CSS classes to apply to the wrapper element.
+       */
+      wrapper?: string | string[]
+
+      /**
+       * CSS classes to apply to the list element.
+       */
+      list?: string | string[]
+
+      /**
+       * CSS classes to apply to the link element.
+       */
+      link?: string | string[]
+
+      /**
+       * CSS classes to apply to the ellipsis element.
+       */
+      ellipsis?: string | string[]
+
+      /**
+       * CSS classes to apply to the buttons element.
+       */
+      buttons?: string | string[]
+
+      /**
+       * CSS classes to apply to the button element.
+       */
+      button?: string | string[]
+    }
+  }>(),
+  {
+    rounded: undefined,
+    color: undefined,
+    currentPage: 1,
+    maxLinksDisplayed: 3,
+    routerQueryKey: 'page',
+    previousIcon: 'lucide:chevron-left',
+    nextIcon: 'lucide:chevron-right',
+    firstIcon: 'lucide:chevron-first',
+    lastIcon: 'lucide:chevron-last',
+    ellipsis: '…',
+    classes: () => ({}),
+  },
+)
+const emits = defineEmits<{
+  'update:currentPage': [currentPage: number]
+}>()
+
+const color = useNuiDefaultProperty(props, 'BasePagination', 'color')
+const rounded = useNuiDefaultProperty(props, 'BasePagination', 'rounded')
+
+const radiuses = {
+  none: '',
+  sm: 'nui-pagination-rounded-sm',
+  md: 'nui-pagination-rounded-md',
+  lg: 'nui-pagination-rounded-lg',
+  full: 'nui-pagination-rounded-full',
+}
+
+const colors = {
+  primary: 'nui-pagination-primary',
+  dark: 'nui-pagination-dark',
+  black: 'nui-pagination-black',
+}
+
+const route = useRoute()
+const lastPage = computed(
+  () => Math.ceil(props.totalItems / props.itemPerPage) || 1,
+)
+const totalPageDisplayed = computed(() =>
+  lastPage.value > props.maxLinksDisplayed + 2
+    ? props.maxLinksDisplayed + 2
+    : lastPage.value,
+)
+const pages = computed(() => {
+  const _pages = []
+  let firstButton = props.currentPage - Math.floor(totalPageDisplayed.value / 2)
+  let lastButton =
+    firstButton +
+    (totalPageDisplayed.value - Math.ceil(totalPageDisplayed.value % 2))
+
+  if (firstButton < 1) {
+    firstButton = 1
+    lastButton = firstButton + (totalPageDisplayed.value - 1)
+  }
+
+  if (lastButton > lastPage.value) {
+    lastButton = lastPage.value
+    firstButton = lastButton - (totalPageDisplayed.value - 1)
+  }
+
+  for (let page = firstButton; page <= lastButton; page += 1) {
+    if (page === firstButton || page === lastButton) {
+      continue
+    }
+
+    _pages.push(page)
+  }
+
+  return _pages
+})
+
+const showLastLink = computed(() => lastPage.value > 1)
+
+const paginatedLink = (page = 1) => {
+  if (props.noRouter) {
+    return {}
+  }
+
+  const _page = Math.max(1, Math.min(page, lastPage.value))
+  const query: any = {
+    ...route.query,
+  }
+
+  if (props.routerQueryKey) {
+    query[props.routerQueryKey] = _page <= 1 ? undefined : _page
+  }
+
+  return {
+    query,
+  } satisfies RouteLocationRaw
+}
+const handleLinkClick = (e: MouseEvent, page = 1) => {
+  const _page = Math.max(1, Math.min(page, lastPage.value))
+  emits('update:currentPage', _page)
+
+  if (props.noRouter) {
+    e.preventDefault()
+    e.stopPropagation()
+
+    return false
+  }
+}
+
+function getPaginationOutput(totalItems, itemsPerPage, currentPage) {
+  const startItem = (currentPage - 1) * itemsPerPage + 1;
+  let endItem = currentPage * itemsPerPage;
+  if (endItem > totalItems) {
+    endItem = totalItems;
+  }
+  return `${startItem}-${endItem} of ${totalItems}`;
+}
+</script>
+
+<template>
+  <div class="flex items-center justify-end gap-7" :class="[
+    rounded && radiuses[rounded],
+    color && colors[color],
+    props.classes?.wrapper,
+  ]">
+    <BaseFocusLoop as="ul" class="nui-pagination-list" :class="[rounded && radiuses[rounded], props.classes?.list]">
+      <slot name="before-pagination"></slot>
+      <div v-if="false">
+        <!-- Link -->
+        <li>
+          <NuxtLink :to="paginatedLink(1)" tabindex="0" class="nui-pagination-link" :class="[
+            currentPage === 1 && 'nui-active',
+            rounded && radiuses[rounded],
+            props.classes?.link,
+          ]" @keydown.space="(e: any) => (e.target as HTMLAnchorElement).click()"
+            @click="(e: any) => handleLinkClick(e, 1)">
+            1
+          </NuxtLink>
+        </li>
+
+        <!-- Ellipsis -->
+        <li v-if="showLastLink && pages.length > 0 && pages[0] > 2">
+          <span class="nui-pagination-ellipsis" :class="[rounded && radiuses[rounded], props.classes?.ellipsis]">
+            {{ props.ellipsis }}
+          </span>
+        </li>
+
+        <!-- Link -->
+        <li v-for="page in pages" :key="page">
+          <NuxtLink :to="paginatedLink(page)" tabindex="0" :aria-current="currentPage === page ? 'page' : undefined"
+            class="nui-pagination-link" :class="[
+              currentPage === page && 'nui-active',
+              rounded && radiuses[rounded],
+              props.classes?.link,
+            ]" @keydown.space="(e: any) => (e.target as HTMLAnchorElement).click()"
+            @click="(e: any) => handleLinkClick(e, page)">
+            {{ page }}
+          </NuxtLink>
+        </li>
+
+        <!-- Ellipsis -->
+        <li v-if="showLastLink && pages[pages.length - 1] < lastPage - 1">
+          <span class="nui-pagination-ellipsis" :class="[rounded && radiuses[rounded], props.classes?.ellipsis]">
+            {{ props.ellipsis }}
+          </span>
+        </li>
+
+        <!-- Link -->
+        <li v-if="showLastLink">
+          <NuxtLink :to="paginatedLink(lastPage)" tabindex="0" class="nui-pagination-link" :class="[
+            currentPage === lastPage && 'nui-active',
+            rounded && radiuses[rounded],
+            props.classes?.link,
+          ]" @keydown.space="(e: any) => (e.target as HTMLAnchorElement).click()"
+            @click="(e: any) => handleLinkClick(e, lastPage)">
+            {{ lastPage }}
+          </NuxtLink>
+        </li>
+      </div>
+
+      <div>
+        <!-- totalItems: {{ props.totalItems }}
+      itemPerPage: {{ props.itemPerPage }}
+      currentPage: {{ props.currentPage }} -->
+        <p class="text-muted-600 text-sm font-medium pb-[2px]">
+          {{ getPaginationOutput(props.totalItems, props.itemPerPage, props.currentPage) }}
+        </p>
+      </div>
+
+
+      <slot name="after-pagination"></slot>
+    </BaseFocusLoop>
+
+
+
+    <BaseFocusLoop class="pt-1 flex gap-8"
+      :class="[rounded && radiuses[rounded], props.classes?.buttons]">
+      <slot name="before-navigation"></slot>
+      
+      <!-- First Page -->
+      <NuxtLink :to="paginatedLink(1)" tabindex="0" :class="['link-button', { 'disabled': currentPage <= 1 }]"
+        @keydown.space="(e: any) => (e.target as HTMLAnchorElement).click()"
+        @click="(e: any) => handleLinkClick(e, Math.max(currentPage - 1, 1))">
+        <slot name="first-icon">
+          <Icon :name="firstIcon" />
+        </slot>
+      </NuxtLink>
+
+      <!-- Previous -->
+      <NuxtLink :to="paginatedLink(currentPage - 1)" tabindex="0" :class="['link-button', { 'disabled': currentPage <= 1 }]"
+        @keydown.space="(e: any) => (e.target as HTMLAnchorElement).click()"
+        @click="(e: any) => handleLinkClick(e, currentPage - 1)">
+        <slot name="previous-icon">
+          <Icon :name="previousIcon" />
+        </slot>
+      </NuxtLink>
+
+      <!-- Next -->
+      <NuxtLink :to="paginatedLink(currentPage + 1)" tabindex="0" :class="['link-button', { 'disabled': currentPage == lastPage }]"
+        @keydown.space="(e: any) => (e.target as HTMLAnchorElement).click()"
+        @click="(e: any) => handleLinkClick(e, currentPage + 1)">
+        <slot name="next-icon">
+          <Icon :name="nextIcon" />
+        </slot>
+      </NuxtLink>
+
+      <NuxtLink :to="paginatedLink(lastPage)" tabindex="0" :class="['link-button', { 'disabled': currentPage == lastPage }]"
+        @keydown.space="(e: any) => (e.target as HTMLAnchorElement).click()"
+        @click="(e: any) => handleLinkClick(e, currentPage + 1)">
+        <slot name="last-icon">
+          <Icon :name="lastIcon" />
+        </slot>
+      </NuxtLink>
+
+      <slot name="after-navigation"></slot>
+    </BaseFocusLoop>
+  </div>
+</template>
+
+<style scoped>
+.nui-pagination-list {
+  background-color: inherit !important;
+  border-width: 0px !important;
+}
+
+.disabled {
+  pointer-events: none;
+  opacity: 0.5;
+  cursor: not-allowed;
+}
+</style>
--- components/Table/index.vue
+++ components/Table/index.vue
@@ -1,197 +1,36 @@
 <template>
   <div>
+
     <!-- Search Data -->
-    <div class="flex flex-row gap-4 mb-3 items-end justify-between">
-      <div class="flex-auto w-50">
-        <BaseInput
-          v-model="criteria.keyword"
-          icon="lucide:search"
-          size="md"
-          rounded="md"
-          label="ค้นหา"
-          placeholder="ค้นหาด้วยหมายเลขใบแจ้งหนี้"
-        />
+    <div class="flex flex-row mb-3 gap-3 items-end relative">
+      <div class="basis-4/12">
+        <BaseInput v-model="criteria.keyword" icon="lucide:search" size="md" rounded="md" label="ค้นหา"
+          placeholder="ค้นหาด้วยหมายเลขใบแจ้งหนี้" />
       </div>
-      <div class="flex-auto">
-        <Test
-          v-model="criteria.status"
-          label="สถานะ"
-          :properties="{
-            value: 'value',
-            label: 'label',
-          }"
-          :items="statusOption"
-          placeholder="ทั้งหมด"
-          rounded="md"
-          size="md"
-        />
-      </div>
-      <!-- <div>
-        <BaseSelect
-          v-model="perPage"
-          rounded="md"
-          label=""
-          :classes="{
-            wrapper: 'w-full sm:w-40',
-          }"
-        >
-          <option :value="10">10 per page</option>
-          <option :value="25">25 per page</option>
-          <option :value="50">50 per page</option>
-          <option :value="100">100 per page</option>
-        </BaseSelect>
+
+      <!-- Search button -->
+      <!-- <div class="basis-0.5/12">
+        <BaseButton color="primary" size="md" shadow="hover" class="w-full">
+          ค้นหา
+        </BaseButton>
       </div> -->
-      <div class="flex-auto w-20">
-        <BaseInput
-          v-model="criteria.datefrom"
-          icon="line-md:calendar"
-          size="md"
-          readonly
-          rounded="md"
-          label="ระหว่างวันที่"
-          placeholder="วว/ดด/ปปปป"
-          @click="openDateFrom = true"
-        />
-
-        <TairoModal
-          :open="openDateFrom"
-          size="sm"
-          @close="openDateFrom = false"
-        >
-          <template #header>
-            <!-- Header -->
-            <div
-              class="flex flex-col w-full items-center justify-between p-4 md:p-6"
-            >
-              <h3
-                class="font-heading text-muted-900 text-lg font-medium leading-6 dark:text-white mb-2"
-              >
-                เลือกวันที่
-              </h3>
-              <VDatePicker
-                v-model="datePickTemp"
-                mode="date"
-                locale="th"
-                :is-dark="colorMode.preference == 'dark' ? true : false"
-              />
-            </div>
-          </template>
-
-          <template #footer>
-            <!-- Footer -->
-            <div class="p-4 md:p-6">
-              <div class="flex gap-x-2">
-                <BaseButton
-                  @click="openDateFrom = false"
-                  color="muted"
-                  variant="solid"
-                >
-                  ปิด</BaseButton
-                >
-
-                <BaseButton
-                  color="primary"
-                  variant="solid"
-                  @click="
-                    (openDateFrom = false), (criteria.datefrom = datePickTemp)
-                  "
-                >
-                  ตกลง
-                </BaseButton>
-              </div>
-            </div>
-          </template>
-        </TairoModal>
+      <div class="basis-2/12">
+        <Test v-model="criteria.status" label="สถานะ" :properties="{
+          value: 'value',
+          label: 'label',
+        }" :items="statusOption" placeholder="ทั้งหมด" rounded="md" size="md" />
       </div>
-      <div class="flex-auto w-20">
-        <BaseInput
-          v-model="criteria.dateTo"
-          icon="line-md:calendar"
-          size="md"
-          rounded="md"
-          label="ถึงวันที่"
-          placeholder="วว/ดด/ปปปป"
-          @click="openDateTo = true"
-        />
-        <TairoModal :open="openDateTo" size="sm" @close="openDateTo = false">
-          <template #header>
-            <!-- Header -->
-            <div
-              class="flex flex-col w-full items-center justify-between p-4 md:p-6"
-            >
-              <h3
-                class="font-heading text-muted-900 text-lg font-medium leading-6 dark:text-white mb-2"
-              >
-                เลือกวันที่
-              </h3>
-              <VDatePicker
-                v-model="datePickTemp"
-                mode="date"
-                locale="th"
-                :is-dark="colorMode.preference == 'dark' ? true : false"
-              />
-            </div>
-          </template>
 
-          <template #footer>
-            <!-- Footer -->
-            <div class="p-4 md:p-6">
-              <div class="flex gap-x-2">
-                <BaseButton
-                  @click="openDateTo = false"
-                  color="muted"
-                  variant="solid"
-                >
-                  ปิด</BaseButton
-                >
-
-                <BaseButton
-                  color="primary"
-                  variant="solid"
-                  @click="
-                    (openDateTo = false), (criteria.dateTo = datePickTemp)
-                  "
-                >
-                  ตกลง
-                </BaseButton>
-              </div>
-            </div>
-          </template>
-        </TairoModal>
-      </div>
-      <div class="flex-none">
-        <BaseButton color="primary" shadow="hover">
-          <Icon name="line-md:plus" class="-ms-1 size-4" />
-          <span>สร้างใบแจ้งหนี้</span>
-        </BaseButton>
-      </div>
-      <div class="flex-none">
-        <BaseButton
-          color="muted"
-          shadow="hover"
-          data-nui-tooltip="ดาวน์โหลด Excel ไฟล์"
-        >
-          <Icon name="line-md:arrow-close-down" class="size-4" />
-        </BaseButton>
-      </div>
+      
     </div>
 
     <div v-if="!pending && data?.data.length === 0">
-      <BasePlaceholderPage
-        title="ไม่พบผลลัพธ์ที่ตรงกับคำค้นหาของคุณ"
-        subtitle="ลองใช้คำค้นหาอื่นดูสิ"
-      >
+      <BasePlaceholderPage title="ไม่พบผลลัพธ์ที่ตรงกับคำค้นหาของคุณ" subtitle="ลองใช้คำค้นหาอื่นดูสิ">
         <template #image>
-          <img
-            class="block dark:hidden"
-            src="/img/illustrations/placeholders/flat/placeholder-search-4.svg"
-            alt="Placeholder image"
-          />
-          <img
-            class="hidden dark:block"
-            src="/img/illustrations/placeholders/flat/placeholder-search-4-dark.svg"
-            alt="Placeholder image"
-          />
+          <img class="block dark:hidden" src="/img/illustrations/placeholders/flat/placeholder-search-4.svg"
+            alt="Placeholder image">
+          <img class="hidden dark:block" src="/img/illustrations/placeholders/flat/placeholder-search-4-dark.svg"
+            alt="Placeholder image">
         </template>
       </BasePlaceholderPage>
     </div>
@@ -222,12 +61,7 @@
       <TairoTableRow v-for="member in team" :key="member.id">
         <TairoTableCell class="p-4">
           <div class="flex items-center">
-            <BaseCheckbox
-              v-model="selected"
-              :value="`table-4-${member.id}`"
-              rounded="md"
-              color="primary"
-            />
+            <BaseCheckbox v-model="selected" :value="`table-4-${member.id}`" rounded="md" color="primary" />
           </div>
         </TairoTableCell>
 
@@ -256,89 +90,68 @@
         <TairoTableCell>{{ member.rate }}</TairoTableCell>
 
         <TairoTableCell>
-          <BaseTag
-            v-if="member.status === 'Available'"
-            color="success"
-            variant="pastel"
-            rounded="full"
-            class="font-medium"
-          >
+          <BaseTag v-if="member.status === 'Available'" color="success" variant="pastel" rounded="full"
+            class="font-medium">
             {{ member.status }}
           </BaseTag>
 
-          <BaseTag
-            v-else-if="member.status === 'New'"
-            color="info"
-            variant="pastel"
-            rounded="full"
-            class="font-medium"
-          >
+          <BaseTag v-else-if="member.status === 'New'" color="info" variant="pastel" rounded="full" class="font-medium">
             {{ member.status }}
           </BaseTag>
 
-          <BaseTag
-            v-else-if="member.status === 'Hired'"
-            color="warning"
-            variant="pastel"
-            rounded="full"
-            class="font-medium"
-          >
+          <BaseTag v-else-if="member.status === 'Hired'" color="warning" variant="pastel" rounded="full"
+            class="font-medium">
             {{ member.status }}
           </BaseTag>
         </TairoTableCell>
 
         <TairoTableCell spaced>
           <div class="flex justify-center">
-            <BaseDropdown
-              variant="context"
-              label="Dropdown"
-              placement="bottom-end"
-              rounded="md"
-            >
-              <BaseDropdownItem
-                to="#"
-                title="User"
-                text="View details"
-                rounded="md"
-              />
+            <BaseDropdown variant="context" label="Dropdown" placement="bottom-end" rounded="md">
+              <BaseDropdownItem to="#" title="User" text="View details" rounded="md" />
             </BaseDropdown>
           </div>
         </TairoTableCell>
       </TairoTableRow>
     </TairoTable>
 
-    <div class="mt-3">
-      <BasePagination
-        :total-items="data?.total ?? 0"
-        :item-per-page="perPage"
-        :current-page="page"
-        rounded="md"
-      />
+    <div class="mt-3 flex items-center justify-end gap-x-8">
+      <div class="mb-[2px]">
+        <BaseSelect v-model="perPage" rounded="md" size="md" label="" :classes="{
+          wrapper: 'w-full w-40',
+        }">
+          <option :value="10">
+            10 per page
+          </option>
+          <option :value="25">
+            25 per page
+          </option>
+          <option :value="50">
+            50 per page
+          </option>
+          <option :value="100">
+            100 per page
+          </option>
+        </BaseSelect>
+      </div>
+      <CustomBasePagination :total-items="data?.total ?? 0" :item-per-page="perPage" :current-page="page" rounded="md" />
     </div>
+
   </div>
 </template>
 
 <script lang="ts" setup>
-import "v-calendar/style.css";
-
 const router = useRouter();
 const route = useRoute();
 
 const selected = ref([]);
 const selectAll = ref(false);
-const openDateFrom = ref(false);
-const openDateTo = ref(false);
-
-const datePickTemp = ref(new Date());
-
-const colorMode = useColorMode();
 const perPage = ref(10);
-const page = computed(() => parseInt((route.query.page as string) ?? "1"));
+const page = computed(() => parseInt((route.query.page as string) ?? '1'))
+
 
 const criteria = reactive({
   keyword: "",
-  datefrom: "",
-  dateTo: "",
   status: {
     label: "ทั้งหมด",
     value: "",
@@ -411,29 +224,23 @@ watch([criteria.keyword, perPage], () => {
     query: {
       page: undefined,
     },
-  });
-});
+  })
+})
 
 const query = computed(() => {
   return {
     filter: criteria.keyword,
     perPage: perPage.value,
     page: page.value,
-  };
-});
+  }
+})
 
 const { data, pending, error, refresh } = await useFetch(
-  "/api/company/members/",
+  '/api/company/members/',
   {
     query,
-  }
-);
+  },
+)
 </script>
 
-<style scoped>
-.vc-header {
-  font-family: "IBM Plex Sans Thai", sans-serif !important;
-  font-weight: 100;
-  font-style: normal;
-}
-</style>
+<style></style>
